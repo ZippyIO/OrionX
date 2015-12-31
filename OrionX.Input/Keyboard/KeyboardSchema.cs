9be2e462e6af1614a5df81653393e80f4f2c3afe
ZippyIO <ZippyIO@users.noreply.github.co>
2015-12-31 20:32:38 +1100

    .NET 4.5, DeltaTime & Input
    Upgraded OrionX to .NET 4.5 to work with the latest MonoGame dev
    release, added basic DeltaTime support & started implementing Input.

---
+++ OrionX.Input/Keyboard/KeyboardSchema.cs
@@ -0,0 +1,44 @@
+﻿// OrionX Disclaimer
+// ------------------------------------------------------
+// OrionX is licenced under GNU GPL 3.0.
+// Please read the licence file for more information.
+// ------------------------------------------------------
+//  
+// * User: ZippyBling
+// * File: Schema.cs
+// * Date: 31/12/2015
+// * Time: 6:34 PM
+
+using Microsoft.Xna.Framework.Input;
+using OrionX.Input.Enumeration;
+
+namespace OrionX.Input.Keyboard
+{
+    public class KeyboardSchema
+    {
+        public SchemaWASD WASD;
+        public SchemaArrow Arrow;
+        public SchemaList List;
+
+        public void WASDSchema(Keys keyOne, Keys keyTwo, Keys keyThree, Keys keyFour)
+        {
+            keyOne = Keys.W;
+            keyTwo = Keys.S;
+            keyThree = Keys.A;
+            keyFour = Keys.D;
+        }
+
+        public void ArrowSchema(Keys keyOne, Keys keyTwo, Keys keyThree, Keys keyFour)
+        {
+            keyOne = Keys.Up;
+            keyTwo = Keys.Down;
+            keyThree = Keys.Left;
+            keyFour = Keys.Right;
+        }
+
+        public void Initalize()
+        {
+
+        }
+    }
+}
---
+++ OrionX.Math/Time/DeltaTime.cs
@@ -0,0 +1,24 @@
+﻿// OrionX Disclaimer
+// ------------------------------------------------------
+// OrionX is licenced under GNU GPL 3.0.
+// Please read the licence file for more information.
+// ------------------------------------------------------
+//  
+// * User: ZippyBling
+// * File: DeltaTime.cs
+// * Date: 31/12/2015
+// * Time: 7:45 PM
+
+using Microsoft.Xna.Framework;
+
+namespace OrionX.Math.Time
+{
+    public class DeltaTime
+    {
+        public float Delta(GameTime gameTime)
+        {
+            var delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            return delta;
+        }
+    }
+}
